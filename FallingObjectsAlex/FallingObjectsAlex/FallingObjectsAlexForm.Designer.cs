d2e6df37754160c56d437bd3e27ec93c43dd3a1d
s465640 <54953677+s465640@users.noreply.github.com>
2019-10-02 11:30:42 -0400

    Initial commit

---
+++ FallingObjectsAlex/FallingObjectsAlex/FallingObjectsAlexForm.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace FallingObjectsAlex
+{
+    partial class frmFallingObjects
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblInstruction = new System.Windows.Forms.Label();
+            this.btncalculate = new System.Windows.Forms.Button();
+            this.lblInformatiom = new System.Windows.Forms.Label();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblInstruction
+            // 
+            this.lblInstruction.AutoSize = true;
+            this.lblInstruction.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstruction.Location = new System.Drawing.Point(24, 73);
+            this.lblInstruction.Name = "lblInstruction";
+            this.lblInstruction.Size = new System.Drawing.Size(613, 29);
+            this.lblInstruction.TabIndex = 0;
+            this.lblInstruction.Text = "Enter the time (in seconds) you released the object:";
+            this.lblInstruction.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // btncalculate
+            // 
+            this.btncalculate.Font = new System.Drawing.Font("MV Boli", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncalculate.Location = new System.Drawing.Point(294, 202);
+            this.btncalculate.Name = "btncalculate";
+            this.btncalculate.Size = new System.Drawing.Size(143, 27);
+            this.btncalculate.TabIndex = 3;
+            this.btncalculate.Text = "Calculate";
+            this.btncalculate.UseVisualStyleBackColor = true;
+            this.btncalculate.Click += new System.EventHandler(this.Btncalculate_Click);
+            // 
+            // lblInformatiom
+            // 
+            this.lblInformatiom.AutoSize = true;
+            this.lblInformatiom.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInformatiom.Location = new System.Drawing.Point(-1, 334);
+            this.lblInformatiom.Name = "lblInformatiom";
+            this.lblInformatiom.Size = new System.Drawing.Size(485, 25);
+            this.lblInformatiom.TabIndex = 4;
+            this.lblInformatiom.Text = "The height of the object above the ground is:";
+            // 
+            // txtTime
+            // 
+            this.txtTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTime.Location = new System.Drawing.Point(672, 80);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Size = new System.Drawing.Size(100, 21);
+            this.txtTime.TabIndex = 7;
+            this.txtTime.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.Location = new System.Drawing.Point(500, 335);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(29, 20);
+            this.lblAnswer.TabIndex = 8;
+            this.lblAnswer.Text = "??";
+            this.lblAnswer.Click += new System.EventHandler(this.LblAnswer_Click);
+            // 
+            // frmFallingObjects
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::FallingObjectsAlex.Properties.Resources.cartoon;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.txtTime);
+            this.Controls.Add(this.lblInformatiom);
+            this.Controls.Add(this.btncalculate);
+            this.Controls.Add(this.lblInstruction);
+            this.Name = "frmFallingObjects";
+            this.Text = "Falling Objects";
+            this.Load += new System.EventHandler(this.FallingObjectsAlexForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInstruction;
+        private System.Windows.Forms.Button btncalculate;
+        private System.Windows.Forms.Label lblInformatiom;
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.Label lblAnswer;
+    }
+}
+
---
+++ FallingObjectsAlex/FallingObjectsAlex/FallingObjectsAlexForm.cs
@@ -0,0 +1,82 @@
+﻿/*
+ * Created by: Alexandrea Udechukwu
+ * Created on: 20-September-2019
+ * Created for: ICS3U Programming
+ * Assignment #2 - Falling Objects
+ * This program calculates the height an object is from the ground when
+  a certain time is entered in the textbox and when the calculate button is clicked.
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FallingObjectsAlex
+{
+    public partial class frmFallingObjects : Form
+    {
+        public frmFallingObjects()
+        {
+            InitializeComponent();
+        }
+
+        private void FallingObjectsAlexForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnTime_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btncalculate_Click(object sender, EventArgs e)
+        {
+            //declare local variables
+            double time, answer;
+
+            //convert string from each textbox to a double
+            time = double.Parse(txtTime.Text);
+
+            //calculate height of the object above the ground
+            answer = 100 - 0.5 * 9.81 * Math.Pow(time, 2);
+
+            //display the height label with its respective answers
+            this.lblAnswer.Show();
+            this.lblAnswer.Text = Convert.ToString(answer);
+            this.lblAnswer.Text = Convert.ToString(answer) + "metres";
+
+            //if answer lower than zero
+            if (answer < 0)
+            {
+                this.lblAnswer.Text="the object has already hit the ground";
+                //if time is lower than zero
+                if (time < 0)
+                    this.lblAnswer.Text = "Please use only positive numbers";
+               
+            } 
+
+     
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LblAnswer_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
